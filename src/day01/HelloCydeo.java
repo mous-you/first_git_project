27fb8a38ce907c20808dd93b19b17f87df11b201
mous-you <mouis73@gmail.com>
2024-12-22 22:19:41 -0500

    Update HelloCydeo.java

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -3,5 +3,9 @@ package day01;
 public class HelloCydeo {
     public static void main(String[] args) {
         System.out.println("Hello Cydeo");
+        System.out.println("Hello Programmers");
+        System.out.println("Hello Students");
+       
+            
     }
 }
